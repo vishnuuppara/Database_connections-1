87a566e0b191ff2c227687fef507b0ca7a2b4342
Sharan Kumar Mudiliyar <Sharanm@KNACKHYD.DCHYD.COM>
2021-11-11 23:19:32 +0530

    Get data with new funtionality

--- GetData.py
+++ GetData.py
@@ -1,19 +1,22 @@
 import mysql.connector
 
+def get_data():
+
+    myconnection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "knack@321",
+        database = "sharan"
+    )
+
+    Dbconnection = myconnection.cursor()
+    sql_query = "select * from my_info"
+    Dbconnection.execute(sql_query)
 
 
-myconnection = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "knack@321",
-    database = "sharan"
-)
 
-Dbconnection = myconnection.cursor()
-sql_query = "select * from my_info"
-Dbconnection.execute(sql_query)
+    for data in Dbconnection:
+        print(data)
 
-
-
-for data in Dbconnection:
-    print(data)
+Data = get_data()
+print(Data)
