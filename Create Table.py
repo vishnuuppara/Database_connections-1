09d14f8944144c417ee6c4c3be7a75c49a641403
vishnuuppara <vishnu.u167@gmail.com>
2021-11-25 13:32:55 +0530

    Added

--- Create Table.py
+++ Create Table.py
@@ -12,3 +12,5 @@ table_name = "my_info"
 connection.execute(f"create table {table_name} ( sno int(100) primary key, name varchar (50), age int (100), address varchar (200))")
 
 print(f"{table_name} table is created successfully")
+
+"This is my contribution....."
